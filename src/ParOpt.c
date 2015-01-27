534c9a8d8c9be467c6c734b33fd6789590448433
gkennedy <devnull@localhost>
2015-01-27 17:15:23 -0500

    removed check at every GMRES iteration

--- src/ParOpt.c
+++ src/ParOpt.c
@@ -2959,11 +2959,6 @@ int ParOpt::optimize( const char * checkpoint ){
       computeKKTStep(ztemp, s_qn, y_qn, wtemp);
     }
 
-    if (gmres_iters > 0){
-      checkKKTStep(gmres_iters > 0);
-    }
-    
-
     // Check the KKT step
     if (k == major_iter_step_check){
       checkKKTStep(gmres_iters > 0);
