50a48accdbfe42f6ec24da5bc92649f39ceb27fd
gkennedy <devnull@localhost>
2016-09-16 14:20:47 -0400

    added a python example with a randomly generated quadratic objective with a specified eigenspectrum

---
+++ examples/python/random_quadratic.py
@@ -0,0 +1,119 @@
+# Import numpy
+import numpy as np
+import mpi4py.MPI as MPI
+
+# Import ParOpt
+from paropt import ParOpt
+
+# Create the rosenbrock function class
+class Quadratic(ParOpt.pyParOptProblem):
+    def __init__(self, A, b, Acon, bcon):
+        # Set the communicator pointer
+        self.comm = MPI.COMM_WORLD
+        self.nvars = len(b)
+        self.ncon = 1
+
+        # Record the quadratic terms
+        self.A = A
+        self.b = b
+        self.Acon = Acon
+        self.bcon = bcon
+
+        # The design history file
+        self.x_hist = []
+
+        # Initialize the base class
+        super(Quadratic, self).__init__(self.comm, self.nvars, self.ncon)
+
+        return
+
+    def getVarsAndBounds(self, x, lb, ub):
+        '''Set the values of the bounds'''
+        x[:] = -2.0 + np.random.uniform(size=len(x))
+        lb[:] = -5.0
+        ub[:] = 5.0
+        return
+
+    def evalObjCon(self, x):
+        '''Evaluate the objective and constraint'''
+        # Append the point to the solution history
+        self.x_hist.append(np.array(x))
+
+        # Evaluate the objective and constraints
+        fail = 0
+        con = np.zeros(1)
+        
+        fobj = 0.5*np.dot(x, np.dot(self.A, x)) + np.dot(self.b, x)
+        con[0] = np.dot(x, self.Acon) + self.bcon
+        return fail, fobj, con
+
+    def evalObjConGradient(self, x, g, A):
+        '''Evaluate the objective and constraint gradient'''
+        fail = 0
+        
+        # The objective gradient
+        g[:] = np.dot(self.A, x) + self.b
+        
+        # The constraint gradient
+        A[0,:] = self.Acon[:]
+
+        return fail
+
+def create_random_problem(eigs):
+    '''
+    Create a random positive definite matrix with the given
+    eigenvalues
+    '''
+
+    # The dimension of the matrix
+    n = len(eigs)
+
+    # Create a random square (n x n) matrix
+    B = np.random.uniform(size=(n, n))
+
+    # Orthogonalize the columns of B so that Q = range(B) = R^{n}
+    Q, s, v = np.linalg.svd(B)
+
+    # Compute A = Q*diag(eigs)*Q^{T}
+    A = np.dot(Q, np.dot(np.diag(eigs), Q.T))
+
+    return A
+
+def solve_problem(eigs):
+    # Get the A matrix
+    A = create_random_problem(eigs)
+
+    # Create the other problem data
+    b = np.random.uniform(size=len(eigs))
+    Acon = np.random.uniform(size=len(eigs))
+    bcon = np.random.uniform()
+
+    problem = Quadratic(A, b, Acon, bcon)
+
+    # Set up the optimization problem
+    max_lbfgs = 20
+    opt = ParOpt.pyParOpt(problem, max_lbfgs, ParOpt.BFGS)
+    opt.optimize()
+
+    return
+
+# Set the eigenvalues for the matrix
+n = 5
+eig_min = 1.0
+eig_max = 1e5
+
+# Solve the problem with linear spacing of eigenvalues
+eigs_linear = np.linspace(eig_min, eig_max, n)
+
+# Solve the problem with a clustered spacing of the eigenvalues
+eigs_clustered = np.zeros(n)
+for i in xrange(1,n+1):
+    u = (1.0*n)/(n-1)*(1.0/(n + 1 - i) - 1.0/n)
+    eigs_clustered[i-1] = eig_min + (eig_max - eig_min)*u
+
+# Solve the two problem types
+print 'Linear spectrum of eigenvalue\n\n'
+solve_problem(eigs_linear)
+
+print '\n\nClustered eigenvalue spectrum\n\n'
+solve_problem(eigs_clustered)
--- examples/python/rosenbrock.py
+++ examples/python/rosenbrock.py
@@ -1,5 +1,7 @@
+# Import some utilities
 import numpy as np
 import mpi4py.MPI as MPI
+import matplotlib.pyplot as plt
 
 # Import ParOpt
 from paropt import ParOpt
@@ -12,6 +14,9 @@ class Rosenbrock(ParOpt.pyParOptProblem):
         self.nvars = 2
         self.ncon = 1
 
+        # The design history file
+        self.x_hist = []
+
         # Initialize the base class
         super(Rosenbrock, self).__init__(self.comm, self.nvars, self.ncon)
 
@@ -19,17 +24,21 @@ class Rosenbrock(ParOpt.pyParOptProblem):
 
     def getVarsAndBounds(self, x, lb, ub):
         '''Set the values of the bounds'''
-        x[:] = 0.25
+        x[:] = -2.0 + np.random.uniform(size=len(x))
         lb[:] = -2.0
         ub[:] = 2.0
         return
 
     def evalObjCon(self, x):
         '''Evaluate the objective and constraint'''
+        # Append the point to the solution history
+        self.x_hist.append(np.array(x))
+
+        # Evaluate the objective and constraints
         fail = 0
         con = np.zeros(1)
         fobj = 100*(x[1]-x[0]**2)**2 + (1-x[0])**2
-        con[0] = x[0] + x[1]
+        con[0] = x[0] + x[1] + 5.0
         return fail, fobj, con
 
     def evalObjConGradient(self, x, g, A):
@@ -45,19 +54,61 @@ class Rosenbrock(ParOpt.pyParOptProblem):
         A[0,1] = 1.0
         return fail
 
+def plot_it_all(problem):
+    '''
+    Plot a carpet plot with the search histories for steepest descent,
+    conjugate gradient and BFGS from the same starting point.
+    '''
+
+    # Set up the optimization problem
+    max_lbfgs = 20
+    opt = ParOpt.pyParOpt(problem, max_lbfgs, ParOpt.BFGS)
+
+    # Create the data for the carpet plot
+    n = 150
+    xlow = -4.0
+    xhigh = 4.0
+    x1 = np.linspace(xlow, xhigh, n)
+    r = np.zeros((n, n))
+
+    for j in xrange(n):
+        for i in xrange(n):
+            fail, fobj, con = problem.evalObjCon([x1[i], x1[j]])
+            r[j, i] = fobj
+
+    # Assign the contour levels
+    levels = np.min(r) + np.linspace(0, 1.0, 75)**2*(np.max(r) - np.min(r))
+
+    # Create the plot
+    fig = plt.figure(facecolor='w')
+    plt.contour(x1, x1, r, levels)
+
+    colours = ['-bo', '-ko', '-co', '-mo', '-yo',
+               '-bx', '-kx', '-cx', '-mx', '-yx' ]
+
+    for k in xrange(len(colours)):
+        # Optimize the problem
+        problem.x_hist = []
+        opt.resetQuasiNewtonHessian()
+        opt.setInitBarrierParameter(0.1)
+        opt.setUseLineSearch(1)
+        opt.optimize()
+
+        # Copy out the steepest descent points
+        sd = np.zeros((2, len(problem.x_hist)))
+        for i in xrange(len(problem.x_hist)):
+            sd[0, i] = problem.x_hist[i][0]
+            sd[1, i] = problem.x_hist[i][1]
+
+        plt.plot(sd[0, :], sd[1, :], colours[k],
+                 label='SD %d'%(sd.shape[1]))
+        plt.plot(sd[0, -1], sd[1, -1], '-ro')
+
+    plt.legend()
+    plt.axis([xlow, xhigh, xlow, xhigh])
+    plt.show()
+
 # Create the Rosenbrock problem class
 rosen = Rosenbrock()
 
-# Create the optimizer
-max_lbfgs = 20
-opt = ParOpt.pyParOpt(rosen, max_lbfgs, ParOpt.BFGS)
-
-# Set optimization parameters
-opt.setUseHvecProduct(0)
-opt.setMajorIterStepCheck(45)
-opt.setMaxMajorIterations(1500)
-
-# opt.checkGradients(1e-6)
-# opt.setGradientCheckFrequency(5, 1e-6)
-
-opt.optimize()
+plot_it_all(rosen)
