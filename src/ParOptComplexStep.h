8940551da212c4535cceda63750911dd16f3a866
aaronyicongfu <fuyicong1996@gmail.com>
2022-03-30 14:52:43 -0400

    use include guard to prevent redefinition for fabs()

--- src/ParOptComplexStep.h
+++ src/ParOptComplexStep.h
@@ -23,11 +23,14 @@ inline double ParOptRealPart( const double& r ){
 }
 
 // Compute the absolute value
+#ifndef FABS_COMPLEX_IS_DEFINED  //prevent redefinition
+#define FABS_COMPLEX_IS_DEFINED
 inline std::complex<double> fabs( const std::complex<double>& c ){
   if (real(c) < 0.0){
     return -c;
   }
   return c;
 }
+#endif // FABS_COMPLEX_IS_DEFINED
 
 #endif // PAROPT_COMPLEX_STEP_H
