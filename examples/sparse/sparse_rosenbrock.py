eac31a8dafecc79d927004263542d432cdcf1343
Yicong Fu <fuyicong1996@gmail.com>
2023-05-11 10:57:22 -0400

    add a rosenbrock example using sparse constraints

---
+++ examples/sparse/sparse_rosenbrock.py
@@ -0,0 +1,147 @@
+# Import some utilities
+import numpy as np
+import mpi4py.MPI as MPI
+import matplotlib.pyplot as plt
+
+# Import ParOpt
+from paropt import ParOpt
+
+
+# Create the rosenbrock function class
+class Rosenbrock(ParOpt.Problem):
+    def __init__(self):
+        # Set the communicator pointer
+        self.comm = MPI.COMM_WORLD
+        self.nvars = 2
+        self.ncon = 0
+        self.nwcon = 1
+        self.nwblock = 1
+
+        # The design history file
+        self.x_hist = []
+
+        # Initialize the base class
+        super(Rosenbrock, self).__init__(
+            comm=self.comm,
+            nvars=self.nvars,
+            ncon=self.ncon,
+            nwcon=self.nwcon,
+            nwblock=self.nwblock,
+        )
+        return
+
+    def getVarsAndBounds(self, x, lb, ub):
+        """Set the values of the bounds"""
+        x[:] = -2.0 + np.random.uniform(size=len(x))
+        lb[:] = -2.0
+        ub[:] = 2.0
+        return
+
+    def evalObjCon(self, x):
+        """Evaluate the objective and constraint"""
+        # Append the point to the solution history
+        self.x_hist.append(np.array(x))
+
+        # Evaluate the objective and dense constraints
+        fail = 0
+        con = np.zeros(1)
+        fobj = 100 * (x[1] - x[0] ** 2) ** 2 + (1 - x[0]) ** 2
+        return fail, fobj, con
+
+    def evalObjConGradient(self, x, g, A):
+        """Evaluate the objective and constraint gradient"""
+        fail = 0
+
+        # The objective gradient
+        g[0] = 200 * (x[1] - x[0] ** 2) * (-2 * x[0]) - 2 * (1 - x[0])
+        g[1] = 200 * (x[1] - x[0] ** 2)
+
+        # Evaluate the dense constraint gradient, if any
+        return fail
+
+    def evalSparseCon(self, x, con):
+        """Evaluate the sparse constraints"""
+        con[0] = x[0] + x[1] + 5.0
+        return
+
+    def addSparseJacobian(self, alpha, x, px, con):
+        """Compute the Jacobian-vector product con += alpha*J(x)*px"""
+        con[0] += alpha * (px[0] + px[1])
+        return
+
+    def addSparseJacobianTranspose(self, alpha, x, pz, out):
+        """Compute the transpose Jacobian-vector product out += alpha*J^{T}*pz"""
+        out[0] += alpha * pz[0]
+        out[1] += alpha * pz[0]
+        return
+
+    def addSparseInnerProduct(self, alpha, x, c, A):
+        """Add the results from the product alpha * J(x)*C*J(x)^{T} to A"""
+        A[0] += alpha * (c[0] + c[1])
+        return
+
+
+def plot_it_all(problem):
+    """
+    Plot a carpet plot with the search histories for steepest descent,
+    conjugate gradient and BFGS from the same starting point.
+    """
+
+    # Create the data for the carpet plot
+    n = 150
+    xlow = -4.0
+    xhigh = 4.0
+    x1 = np.linspace(xlow, xhigh, n)
+    r = np.zeros((n, n))
+
+    for j in range(n):
+        for i in range(n):
+            fail, fobj, con = problem.evalObjCon([x1[i], x1[j]])
+            r[j, i] = fobj
+
+    # Assign the contour levels
+    levels = np.min(r) + np.linspace(0, 1.0, 75) ** 2 * (np.max(r) - np.min(r))
+
+    # Create the plot
+    fig = plt.figure(facecolor="w", figsize=(6.0, 6.0))
+    plt.contour(x1, x1, r, levels)
+
+    colours = ["-bo", "-ko", "-co", "-mo", "-yo", "-bx", "-kx", "-cx", "-mx", "-yx"]
+
+    options = {
+        "algorithm": "tr",
+        "tr_init_size": 0.5,
+        "tr_min_size": 1e-6,
+        "tr_max_size": 10.0,
+        "tr_eta": 0.1,
+        "tr_adaptive_gamma_update": True,
+        "tr_max_iterations": 200,
+    }
+
+    for k in range(len(colours)):
+        # Optimize the problem
+        problem.x_hist = []
+
+        opt = ParOpt.Optimizer(rosen, options)
+        opt.optimize()
+
+        # Copy out the steepest descent points
+        sd = np.zeros((2, len(problem.x_hist)))
+        for i in range(len(problem.x_hist)):
+            sd[0, i] = problem.x_hist[i][0]
+            sd[1, i] = problem.x_hist[i][1]
+
+        plt.plot(sd[0, :], sd[1, :], colours[k], label="IP nit:%d" % (sd.shape[1]))
+        plt.plot(sd[0, -1], sd[1, -1], "-ro")
+
+    plt.legend()
+    plt.axis([xlow, xhigh, xlow, xhigh])
+    plt.gca().set_aspect("equal")
+    plt.show()
+
+
+# Create the Rosenbrock problem class
+rosen = Rosenbrock()
+rosen.checkGradients()
+
+plot_it_all(rosen)
